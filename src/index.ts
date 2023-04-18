f11e2eef16f9c636db6d64fc299c50758abef42f
EdwinTJ <edwin16x@gmail.com>
2023-04-18 17:34:39 -0600

    Route

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
